fbab43f58cc54d5ab58bd43293360b91e4b8fc49
John Manero <jmanero@rapid7.com>
2015-07-08 15:49:10 -0400

    Add output capture from Packer to find AMI ids

--- lib/builderator/tasks/packer.rb
+++ lib/builderator/tasks/packer.rb
@@ -2,11 +2,14 @@ require 'thor'
 require 'thor/actions'
 require_relative './berks'
 require_relative '../control/packer'
+require_relative '../util/shell'
 
 module Builderator
   module Tasks
     class Packer < Thor
       include Thor::Actions
+      include Util::Shell
+
       class_option :config, :aliases => :c, :desc => "Path to Berkshelf's config.json"
       class_option :berksfile, :aliases => :b, :desc => 'Path to the Berksfile to use'
 
@@ -25,9 +28,43 @@ module Builderator
       end
 
       desc 'build ARGS', 'Run a build with the installed version of packer'
+      option 'properties-file', :desc => 'Write build outputs to a properties file'
       def build(*args)
         invoke Tasks::Berks, 'vendor', [], options
-        run "#{ Control::Packer.bin } build #{ args.join(' ') }"
+
+        packer_output = execute("#{ Control::Packer.bin } build #{ args.join(' ') }")
+
+        ## Try to find the ID of the new AMI
+        ami_id_search = /AMI: (ami-[0-9a-f]{8})/.match(packer_output.string)
+        if ami_id_search.nil?
+          say_status :failure, 'Unable to find AMI ID from packer build', :red
+          return
+        end
+
+        say_status :success, "Created AMI #{ ami_id_search[1] }"
+        return unless options['properties-file']
+
+        create_file options['properties-file'], '', :force => true
+        append_file options['properties-file'], "image.useast1=#{ ami_id_search[1] }"
+      end
+
+      desc 'find_ami', 'Find the ID of the new AMI from packer output'
+      option 'properties-file', :desc => 'Write build outputs to a properties file'
+      def find_ami
+        tee = Builderator::Util::Shell::BufferTee.new($stdout)
+        $stdin.each { |l| tee.write(l) } # Buffer stdin
+
+        ami_id_search = /AMI: (ami-[0-9a-f]{8})/.match(packer_output.string)
+        if ami_id_search.nil?
+          say_status :failure, 'Unable to find AMI ID from packer build', :red
+          return
+        end
+
+        say_status :success, "Created AMI #{ ami_id_search[1] }"
+        return unless options['properties-file']
+
+        create_file options['properties-file'], '', :force => true
+        append_file options['properties-file'], "image.useast1=#{ ami_id_search[1] }"
       end
     end
   end
