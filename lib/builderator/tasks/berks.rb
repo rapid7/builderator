f4105a9bee4aeba116c54cc05d0e8a7643f4f775
John Manero <jmanero@rapid7.com>
2015-09-03 15:39:06 -0400

    Fix cookbook path resolution
    Refactors and de-duplicates code that locates the cookbook and Berksfile. Now the util method automagically
    checks to see if there's a `cookbook` directory.

--- lib/builderator/tasks/berks.rb
+++ lib/builderator/tasks/berks.rb
@@ -1,54 +1,54 @@
 require 'thor'
 require 'thor/actions'
 require_relative './cookbook'
-require_relative '../control/berks'
+require_relative '../util/cookbook'
 
 module Builderator
   module Tasks
     class Berks < Thor
       include Thor::Actions
       class_option :config, :aliases => :c, :desc => "Path to Berkshelf's config.json"
-      class_option :berksfile, :aliases => :b, :desc => 'Path to the Berksfile to use'
+      class_option :cookbook, :aliases => :b, :desc => 'Path to the cookbook to use'
       class_option :version, :type => :boolean,
                              :default => true,
                              :desc => 'Write current verison to file'
 
       desc "local [PATH = #{ Util::Cookbook::DEFAULT_VENDOR }]", 'Vendor the local cookbook source and its dependencies'
       def local(path = Util::Cookbook::DEFAULT_VENDOR)
+        Util::Cookbook.path(options['cookbook'])
+
         command = 'BERKS_INSTALL_FROM=source'
         command << " berks vendor #{ path }"
         command << " -c #{ options['config'] }" if options.include?('config')
-        command << " -b #{ options['berksfile'] }" if options.include?('berksfile')
+        command << " -b #{ Util::Cookbook.berksfile }"
 
-        remove_file File.expand_path('../Berksfile.lock', Control::Berks.file!(options['berksfile']))
-
-        cookbook_path = options.include?('berksfile') ? File.dirname(options['berksfile']) : './'
-        invoke Tasks::Cookbook, 'metadata', [cookbook_path], options
-
+        remove_file File.expand_path('Berksfile.lock', Util::Cookbook.path)
+        invoke Tasks::Cookbook, 'metadata', [], options
         run command
       end
 
       desc "vendor [PATH = #{ Util::Cookbook::DEFAULT_VENDOR }]", 'Vendor a cookbook release and its dependencies'
       def vendor(path = Util::Cookbook::DEFAULT_VENDOR)
+        Util::Cookbook.path(options['cookbook'])
+
         command = 'BERKS_INSTALL_FROM=release'
         command << " berks vendor #{ path }"
         command << " -c #{ options['config'] }" if options.include?('config')
-        command << " -b #{ options['berksfile'] }" if options.include?('berksfile')
+        command << " -b #{ Util::Cookbook.berksfile }"
 
-        remove_file File.expand_path('../Berksfile.lock', options.fetch('berksfile', Control::Berks.file!))
+        remove_file File.expand_path('Berksfile.lock', Util::Cookbook.path)
         run command
       end
 
       desc 'upload', 'Upload the local cookbook source and its dependencies to the Chef server'
       option 'dry-run', :type => :boolean, :default => false
-      option :path, :default => Util::Cookbook::DEFAULT_VENDOR
-      def upload
+      def upload(path = Util::Cookbook::DEFAULT_VENDOR)
         command = 'BERKS_INSTALL_FROM=source'
         command << " berks upload"
         command << " -c #{ options['config'] }" if options.include?('config')
-        command << " -b #{ options['berksfile'] }" if options.include?('berksfile')
+        command << " -b #{ Util::Cookbook.berksfile }"
 
-        invoke Tasks::Berks, :local, [options.delete('path')], options
+        invoke Tasks::Berks, :local, [path], options
 
         return say_status :dryrun, command if options['dry-run']
         run command
--- lib/builderator/tasks/cookbook.rb
+++ lib/builderator/tasks/cookbook.rb
@@ -10,13 +10,25 @@ module Builderator
                              :default => true,
                              :desc => 'Write current verison to file'
 
-      desc 'metadata [PATH = ./]', 'Use cookbook matadata file at PATH/metadata.rb to generate PATH/matadata.json'
-      def metadata(cookbook_path = './')
+      desc 'metadata [PATH]', 'Use cookbook matadata file at PATH/metadata.rb to generate PATH/matadata.json'
+      def metadata(cookbook = nil)
+        Util::Cookbook.path(cookbook) unless cookbook.nil?
         invoke 'version:current', [], options if options['version']
-        Util::Cookbook.path(cookbook_path)
 
         create_file File.join(Util::Cookbook.path, 'metadata.json'),
                     Util::Cookbook.metadata.to_json, :force => true
+      end
+
+      desc 'package PATH', 'Package a cookbook into a tarball'
+      def package(cookbook = nil)
+        Util::Cookbook.path(cookbook) unless cookbook.nil?
+
+      end
+
+      desc 'publish', 'Publish a cookbook to supermarket.chef.io'
+      def publish(cookbook = nil)
+        Util::Cookbook.path(cookbook) unless cookbook.nil?
+
       end
 
       desc 'version COOKBOOK', 'Print the current version of a vendored cookbook'
