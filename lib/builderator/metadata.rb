9303364ba5e298271e4dde6317100fe84bcd49d6
John Manero <jmanero@rapid7.com>
2015-09-03 15:37:58 -0400

    Some opinionated changes... -> Error message when executed outside of Bundler -> Name of CONSTANT that the gem path is stored in

--- lib/builderator/metadata.rb
+++ lib/builderator/metadata.rb
@@ -1,5 +1,5 @@
 module Builderator
-  GEM_ROOT = File.expand_path('../../..', __FILE__)
-  VERSION = IO.read(File.join(GEM_ROOT, 'VERSION')) rescue '0.0.1'
-  DESCRIPTION = IO.read(File.join(GEM_ROOT, 'README.md')) rescue 'README.md not found!'
+  PATH = File.expand_path('../../..', __FILE__)
+  VERSION = IO.read(File.join(PATH, 'VERSION')) rescue '0.0.1'
+  DESCRIPTION = IO.read(File.join(PATH, 'README.md')) rescue 'README.md not found!'
 end
