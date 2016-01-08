49acd4632c2a2f21f0f303591bcfb403e8beb4ba
John Manero <jmanero@rapid7.com>
2016-01-08 12:12:59 -0500

    Stop using README.md as the gem's description

--- lib/builderator/metadata.rb
+++ lib/builderator/metadata.rb
@@ -3,5 +3,8 @@ require_relative './util'
 # :nodoc:
 module Builderator
   VERSION = Util.source_path('VERSION').read rescue '0.0.1'
-  DESCRIPTION = Util.source_path('README.md').read rescue 'README.md not found!'
+  DESCRIPTION = 'Builderator automates many of the common steps required to build VMs '\
+                'and images with Chef. It provides a common configuration layer for '\
+                'Chef, Berkshelf, Vagrant, and Packer, and tasks to orchestrate the '\
+                'usage of each. https://github.com/rapid7/builderator'
 end
