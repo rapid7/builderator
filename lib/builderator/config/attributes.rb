5260b6ca5b503770b7e6a47910c72bbf7f18571b
John Manero <jmanero@rapid7.com>
2016-03-02 11:53:43 -0500

    Update the attribute definition DSL to use Config::List for list-type values

--- lib/builderator/config/attributes.rb
+++ lib/builderator/config/attributes.rb
@@ -2,6 +2,7 @@ require 'forwardable'
 require 'json'
 
 require_relative './rash'
+require_relative './list'
 
 module Builderator
   module Config
@@ -18,31 +19,30 @@ module Builderator
           # Helpers for Array-type attributes
           ##
           if options[:type] == :list
-            default = Array
+            define_method(attribute_name) do |*arg, **run_options|
+              ## Instantiate List if it doesn't exist yet. `||=` will always return a new Rash.
+              @attributes[attribute_name] = Config::List.new(run_options) unless @attributes.has?(attribute_name, Config::List)
 
-            ## Add an appender DSL method
-            define_method(options[:singular]) do |*args|
-              append_if_valid(attribute_name, args.flatten, default, options)
+              @attributes[attribute_name].set(*arg.flatten) unless arg.empty?
+              @attributes[attribute_name]
+            end
+
+            define_method(options[:singular]) do |*arg, **run_options|
+              send(attribute_name, run_options).push(*arg.flatten)
             end if options.include?(:singular)
+
+            return
           end
 
           ## Getter/Setter
           define_method(attribute_name) do |*arg|
-            arg.flatten!
-            arg = arg.first unless options[:type] == :list
-
-            set_or_return(attribute_name, arg, default, options)
+            set_or_return(attribute_name, arg.first, default, options)
           end
 
           ## Setter
           define_method("#{attribute_name}=") do |arg|
             set_if_valid(attribute_name, arg, options)
-          end unless options[:type] == :list
-        end
-
-        ## Add a method the DSL
-        def define(method_name, &block)
-          define_method(method_name, &block)
+          end
         end
 
         ##
@@ -58,7 +58,7 @@ module Builderator
         # end
         # ```
         #
-        # Multiple calls to the DSL method will are safe and will
+        # Multiple calls to the DSL method are safe and will
         # update the same sub-node.
         ##
         def namespace(namespace_name, &definition)
@@ -207,20 +207,8 @@ module Builderator
         @attributes[key] = arg
       end
 
-      def append_if_valid(key, arg, default = Array, **options)
-        ## TODO: define validation interface
-
-        attribute = set_or_return(key, nil, default, options)
-        arg.reject! { |item| attribute.include?(item) }
-
-        return if arg.empty?
-
-        dirty(true) ## A mutation has occured
-        attribute.push(*arg)
-      end
-
       def set_or_return(key, arg = nil, default = nil, **options)
-        if arg.nil? || (arg.is_a?(Array) && arg.empty?)
+        if arg.nil?
           return @attributes[key] if @attributes.has?(key)
 
           ## Default
