e564d066a359212e48a56277b96aaccdd6a58ff5
John Manero <jmanero@rapid7.com>
2016-04-20 15:57:58 -0400

    Update the embedded berkshelf version to ~> 4.3
    Berkshelf 4 upgraded the solve library to major version 2. This changed default behavior in such a was that
    broke resolution of circular cookbook dependencies. We need to explicitly install dep_selector and specify the
    gecode solver engine.

--- lib/builderator/patch/berkshelf.rb
+++ lib/builderator/patch/berkshelf.rb
@@ -1,10 +1,10 @@
+# Monkey patch to handle the case where Content-Type headers returned by servers
+# say "gzip" by the resulting message body isn't in gzip format.
+# TODO Remove this when we upgrade Berkshelf and the gzip response goes away.
+# https://github.com/berkshelf/berkshelf-api-client/blob/v1.3.0/lib/berkshelf/api_client/connection.rb#L37
 require 'faraday_middleware'
 
 module FaradayMiddleware
-  # Monkey patch to handle the case where Content-Type headers returned by servers
-  # say "gzip" by the resulting message body isn't in gzip format.
-  # TODO Remove this when we upgrade Berkshelf and the gzip response goes away.
-  # https://github.com/berkshelf/berkshelf-api-client/blob/v1.3.0/lib/berkshelf/api_client/connection.rb#L37
   class Gzip
     alias_method :__uncompress_gzip__, :uncompress_gzip
     def uncompress_gzip(body)
@@ -16,3 +16,8 @@ module FaradayMiddleware
 end
 
 Faraday::Response.register_middleware :gzip => FaradayMiddleware::Gzip
+
+# Force Berkshelf to use the gecode dependency solver
+require 'solve'
+
+Solve.engine = :gecode
